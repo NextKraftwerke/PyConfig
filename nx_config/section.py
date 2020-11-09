1a366687af749f7ef36c98518698a6b41a20f8cf
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2020-11-09 17:46:50 +0100

    Started implementing pretty printing for sections.

--- nx_config/section.py
+++ nx_config/section.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import Any
+
 # noinspection PyProtectedMember
 from nx_config._core.naming_utils import (
     mutable_section_attr as _mutable_attr,
@@ -5,6 +8,22 @@ from nx_config._core.naming_utils import (
 )
 # noinspection PyProtectedMember
 from nx_config._core.section_meta import SectionMeta as _Meta
+
+
+def _base_value2str(value: Any) -> str:
+    if isinstance(value, (str, Path)):
+        return f"'{value}'"
+
+    return str(value)
+
+
+def _value2str(value: Any) -> str:
+    if isinstance(value, frozenset):
+        return "{" + ", ".join((_base_value2str(x) for x in value)) + "}"
+    elif isinstance(value, tuple):
+        return "(" + ", ".join((_base_value2str(x) for x in value)) + ")"
+
+    return _base_value2str(value)
 
 
 class ConfigSection(metaclass=_Meta):
@@ -16,3 +35,9 @@ class ConfigSection(metaclass=_Meta):
         for entry_name in getattr(type(self), "__annotations__", {}):
             entry = getattr(type(self), entry_name)
             setattr(self, _internal_name(entry_name), entry.default)
+
+    def __str__(self):
+        entry_names = type(self).__annotations__.keys()
+        entries = ((x, _value2str(getattr(self, x))) for x in entry_names)
+        attrs_str = ", ".join((f"{k}={v}" for k, v in entries))
+        return f"{type(self).__name__}({attrs_str})"
--- tests/test_pretty_printing.py
+++ tests/test_pretty_printing.py
@@ -27,11 +27,28 @@ class PrettyPrintingTestCase(TestCase):
             cats: Optional[FrozenSet[str]] = frozenset(("grey", "brown", "black", "white"))
             dogs: Tuple[str, ...] = ("lazy", "happy", "sad")
             sasquatch: Tuple[str, ...] = ()
+            big_foot: FrozenSet[str] = frozenset()
+            secret_files: FrozenSet[Path] = frozenset((Path("hi.txt"), Path("/hello.md")))
+            more_files: Tuple[Path, ...] = (Path("bye.exe"), Path("/see_ya.py"))
+            fibonacci: FrozenSet[int] = frozenset((0, 1, 1, 2, 3, 5))
+            more_fibonacci: Tuple[int, ...] = (8, 13, 21, 34)
+            holidays: FrozenSet[datetime] = frozenset((datetime(1985, 11, 12),))
+            non_holidays: Tuple[datetime, ...] = (datetime(1985, 11, 13),)
+            old_tokens: FrozenSet[UUID] = frozenset((UUID(int=1), UUID(int=9_999_999_999_999)))
+            future_tokens: Tuple[UUID, ...] = (UUID(int=2), UUID(int=7), UUID(int=17))
 
         sec = DatabaseSection()
 
         cats_str = "{" + ", ".join(f"'{x}'" for x in sec.cats) + "}"
         dogs_str = "(" + ", ".join(f"'{x}'" for x in sec.dogs) + ")"
+        big_foot_str = "{}"
+        secret_files_str = "{" + ", ".join(f"'{x}'" for x in sec.secret_files) + "}"
+        more_files_str = "(" + ", ".join(f"'{x}'" for x in sec.more_files) + ")"
+        holidays_str = "{" + ", ".join(str(x) for x in sec.holidays) + "}"
+        non_holidays_str = "(" + ", ".join(str(x) for x in sec.non_holidays) + ")"
+        old_tokens_str = "{" + ", ".join(str(x) for x in sec.old_tokens) + "}"
+        future_tokens_str = "(" + ", ".join(str(x) for x in sec.future_tokens) + ")"
+
         self.assertEqual(
             str(sec),
             (
@@ -49,13 +66,26 @@ class PrettyPrintingTestCase(TestCase):
                 f" growth_factor={sec.growth_factor},"
                 f" cats={cats_str},"
                 f" dogs={dogs_str},"
-                f" sasquatch=())"
+                f" sasquatch=(),"
+                f" big_foot={big_foot_str},"
+                f" secret_files={secret_files_str},"
+                f" more_files={more_files_str},"
+                f" fibonacci={set(sec.fibonacci)},"
+                f" more_fibonacci={sec.more_fibonacci},"
+                f" holidays={holidays_str},"
+                f" non_holidays={non_holidays_str},"
+                f" old_tokens={old_tokens_str},"
+                f" future_tokens={future_tokens_str}"
+                f")"
             ),
         )
 
+    # TODO: Empty section.
     # TODO: Newlines in string values should become '\n' and not actual newlines.
     # TODO: repr should be multiline.
     # TODO: Config str (single line).
     # TODO: Config repr (multiline).
     # TODO: SecretString masking in section.
     # TODO: SecretString masking in config.
+    # TODO: SecretString masking in tuple.
+    # TODO: SecretString masking in frozenset.
