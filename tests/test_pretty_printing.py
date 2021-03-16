0cbcfa44b70de86b4b9e0788b84f319909b3c75f
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-03-16 13:06:11 +0100

    Added tests for builtin tuple/frozenset support and made them pass.

--- tests/test_pretty_printing.py
+++ tests/test_pretty_printing.py
@@ -1,62 +1,72 @@
 from datetime import datetime, timezone, timedelta
 from pathlib import Path
-from typing import Optional, FrozenSet, Tuple
+from typing import Optional
 from unittest import TestCase
 from uuid import UUID
 
 from nx_config import ConfigSection, URL, SecretString, validate, Config
 from nx_config.test_utils import mutable_config
+from tests.typing_test_helpers import collection_type_holders, CollectionTypeHolder
 
 
-class DatabaseSection(ConfigSection):
-    user: str = "John Doe"
-    password: SecretString
-    token: UUID = UUID(int=5_444_333_222_111)
-    birthday: datetime = datetime(
-        1955, 11, 5, 1, 22, tzinfo=timezone(offset=timedelta(hours=-3))
-    )
-    url: URL = "www.nx_config_db.com"
-    port: int = 1_234
-    ultimate_question: Optional[str] = None
-    no_collection: Optional[Tuple[int, ...]] = None
-    resources: Path = Path("/a/b/c/resources/")
-    rel_resources: Path = Path("c/resources")
-    secure_mode: bool = True
-    growth_factor: float = 1.5
-    cats: Optional[FrozenSet[str]] = frozenset(("grey", "brown", "black", "white"))
-    dogs: Tuple[str, ...] = ("lazy", "happy", "sad")
-    sasquatch: Tuple[str, ...] = ()
-    big_foot: FrozenSet[str] = frozenset()
-    secret_files: FrozenSet[Path] = frozenset((Path("hi.txt"), Path("/hello.md")))
-    more_files: Tuple[Path, ...] = (Path("bye.exe"), Path("/see_ya.py"))
-    fibonacci: FrozenSet[int] = frozenset((0, 1, 1, 2, 3, 5))
-    more_fibonacci: Tuple[int, ...] = (8, 13, 21, 34)
-    holidays: FrozenSet[datetime] = frozenset((datetime(1985, 11, 12),))
-    non_holidays: Tuple[datetime, ...] = (datetime(1985, 11, 13),)
-    old_tokens: FrozenSet[UUID] = frozenset((UUID(int=1), UUID(int=9_999_999_999_999)))
-    future_tokens: Tuple[UUID, ...] = (UUID(int=2), UUID(int=7), UUID(int=17))
-    needs_escaping: str = "Hello\nWorld\r!\tHowdy?"
+def _get_testing_section_cls(tps: CollectionTypeHolder) -> type:
+    # noinspection PyUnresolvedReferences
+    class DatabaseSection(ConfigSection):
+        user: str = "John Doe"
+        password: SecretString
+        token: UUID = UUID(int=5_444_333_222_111)
+        birthday: datetime = datetime(
+            1955, 11, 5, 1, 22, tzinfo=timezone(offset=timedelta(hours=-3))
+        )
+        url: URL = "www.nx_config_db.com"
+        port: int = 1_234
+        ultimate_question: Optional[str] = None
+        no_collection: Optional[tps.tuple[int, ...]] = None
+        resources: Path = Path("/a/b/c/resources/")
+        rel_resources: Path = Path("c/resources")
+        secure_mode: bool = True
+        growth_factor: float = 1.5
+        cats: Optional[tps.frozenset[str]] = frozenset(("grey", "brown", "black", "white"))
+        dogs: tps.tuple[str, ...] = ("lazy", "happy", "sad")
+        sasquatch: tps.tuple[str, ...] = ()
+        big_foot: tps.frozenset[str] = frozenset()
+        secret_files: tps.frozenset[Path] = frozenset((Path("hi.txt"), Path("/hello.md")))
+        more_files: tps.tuple[Path, ...] = (Path("bye.exe"), Path("/see_ya.py"))
+        fibonacci: tps.frozenset[int] = frozenset((0, 1, 1, 2, 3, 5))
+        more_fibonacci: tps.tuple[int, ...] = (8, 13, 21, 34)
+        holidays: tps.frozenset[datetime] = frozenset((datetime(1985, 11, 12),))
+        non_holidays: tps.tuple[datetime, ...] = (datetime(1985, 11, 13),)
+        old_tokens: tps.frozenset[UUID] = frozenset((UUID(int=1), UUID(int=9_999_999_999_999)))
+        future_tokens: tps.tuple[UUID, ...] = (UUID(int=2), UUID(int=7), UUID(int=17))
+        needs_escaping: str = "Hello\nWorld\r!\tHowdy?"
 
-    class Nested:
-        pass
+        class Nested:
+            pass
 
-    Alias = Nested
+        Alias = Nested
 
-    def a_method(self):
-        pass
+        def a_method(self):
+            pass
 
-    @validate
-    def a_validator(self):
-        pass
+        @validate
+        def a_validator(self):
+            pass
+
+    return DatabaseSection
 
 
 class EmptySection(ConfigSection):
     pass
 
 
-class BigConfig(Config):
-    database: DatabaseSection
-    empty: EmptySection
+def _get_big_testing_config_cls(tps: CollectionTypeHolder) -> type:
+    db_section = _get_testing_section_cls(tps)
+
+    class BigConfig(Config):
+        database: db_section
+        empty: EmptySection
+
+    return BigConfig
 
 
 class EmptyConfig(Config):
@@ -69,7 +79,8 @@ def indent_after_newline(s: str) -> str:
 
 class PrettyPrintingTestCase(TestCase):
     def setUp(self) -> None:
-        database_sec = DatabaseSection()
+        db_section_cls = _get_testing_section_cls(collection_type_holders[0])
+        database_sec = db_section_cls()
 
         cats_str = "{" + ", ".join(f"'{x}'" for x in database_sec.cats) + "}"
         dogs_str = "(" + ", ".join(f"'{x}'" for x in database_sec.dogs) + ")"
@@ -142,40 +153,48 @@ class PrettyPrintingTestCase(TestCase):
         )
 
     def test_pretty_section_str(self):
-        sec = DatabaseSection()
-        self.assertEqual(str(sec), self.expected_database_str)
+        for tps in collection_type_holders:
+            with self.subTest(types=tps):
+                sec = _get_testing_section_cls(tps)()
+                self.assertEqual(str(sec), self.expected_database_str)
 
     def test_pretty_empty_section_str(self):
         sec = EmptySection()
         self.assertEqual(str(sec), "EmptySection()")
 
     def test_pretty_section_repr(self):
-        sec = DatabaseSection()
-        self.assertEqual(repr(sec), self.expected_database_repr)
+        for tps in collection_type_holders:
+            with self.subTest(types=tps):
+                sec = _get_testing_section_cls(tps)()
+                self.assertEqual(repr(sec), self.expected_database_repr)
 
     def test_pretty_empty_section_repr(self):
         sec = EmptySection()
         self.assertEqual(repr(sec), "EmptySection(\n)")
 
     def test_pretty_config_str(self):
-        cfg = BigConfig()
-        self.assertEqual(str(cfg), f"BigConfig(database={cfg.database}, empty={cfg.empty})")
+        for tps in collection_type_holders:
+            with self.subTest(types=tps):
+                cfg = _get_big_testing_config_cls(tps)()
+                self.assertEqual(str(cfg), f"BigConfig(database={cfg.database}, empty={cfg.empty})")
 
     def test_pretty_empty_config_str(self):
         cfg = EmptyConfig()
         self.assertEqual(str(cfg), "EmptyConfig()")
 
     def test_pretty_config_repr(self):
-        cfg = BigConfig()
-        self.assertEqual(
-            repr(cfg),
-            (
-                f"BigConfig(\n"
-                f"    database={indent_after_newline(repr(cfg.database))},\n"
-                f"    empty={indent_after_newline(repr(cfg.empty))},\n"
-                f")"
-            )
-        )
+        for tps in collection_type_holders:
+            with self.subTest(types=tps):
+                cfg = _get_big_testing_config_cls(tps)()
+                self.assertEqual(
+                    repr(cfg),
+                    (
+                        f"BigConfig(\n"
+                        f"    database={indent_after_newline(repr(cfg.database))},\n"
+                        f"    empty={indent_after_newline(repr(cfg.empty))},\n"
+                        f")"
+                    )
+                )
 
     def test_pretty_empty_config_repr(self):
         cfg = EmptyConfig()
@@ -245,42 +264,44 @@ class PrettyPrintingTestCase(TestCase):
         )
 
     def test_secret_string_masking_in_collections(self):
-        class MySection(ConfigSection):
-            my_tuple: Tuple[SecretString, ...]
-            my_frozenset: Optional[FrozenSet[SecretString]] = None
-            my_empty_tuple: Tuple[SecretString, ...] = ()
-            my_empty_frozenset: FrozenSet[SecretString] = frozenset()
-            my_none: Optional[Tuple[SecretString, ...]] = None
+        for tps in collection_type_holders:
+            with self.subTest(types=tps):
+                class MySection(ConfigSection):
+                    my_tuple: tps.tuple[SecretString, ...]
+                    my_frozenset: Optional[tps.frozenset[SecretString]] = None
+                    my_empty_tuple: tps.tuple[SecretString, ...] = ()
+                    my_empty_frozenset: tps.frozenset[SecretString] = frozenset()
+                    my_none: Optional[tps.tuple[SecretString, ...]] = None
 
-        class MyConfig(Config):
-            my_section: MySection
+                class MyConfig(Config):
+                    my_section: MySection
 
-        cfg = MyConfig()
+                cfg = MyConfig()
 
-        with mutable_config(cfg):
-            cfg.my_section.my_tuple = ("hello",)
-            cfg.my_section.my_frozenset = frozenset(("goodbye", "see ya!"))
+                with mutable_config(cfg):
+                    cfg.my_section.my_tuple = ("hello",)
+                    cfg.my_section.my_frozenset = frozenset(("goodbye", "see ya!"))
 
-        self.assertEqual(
-            str(cfg.my_section),
-            (
-                "MySection(my_tuple=('*****', ...), my_frozenset={'*****', ...},"
-                " my_empty_tuple=(), my_empty_frozenset={}, my_none=None)"
-            ),
-        )
+                self.assertEqual(
+                    str(cfg.my_section),
+                    (
+                        "MySection(my_tuple=('*****', ...), my_frozenset={'*****', ...},"
+                        " my_empty_tuple=(), my_empty_frozenset={}, my_none=None)"
+                    ),
+                )
 
-        my_tuple_str = repr((...,)).replace("Ellipsis,", f"{repr('*****')}, ...")
-        my_frozenset_str = repr(frozenset((...,))).replace("Ellipsis", f"{repr('*****')}, ...")
+                my_tuple_str = repr((...,)).replace("Ellipsis,", f"{repr('*****')}, ...")
+                my_frozenset_str = repr(frozenset((...,))).replace("Ellipsis", f"{repr('*****')}, ...")
 
-        self.assertEqual(
-            repr(cfg.my_section),
-            (
-                f"MySection(\n"
-                f"    my_tuple={my_tuple_str},\n"
-                f"    my_frozenset={my_frozenset_str},\n"
-                f"    my_empty_tuple={repr(())},\n"
-                f"    my_empty_frozenset={repr(frozenset())},\n"
-                f"    my_none=None,\n"
-                f")"
-            ),
-        )
+                self.assertEqual(
+                    repr(cfg.my_section),
+                    (
+                        f"MySection(\n"
+                        f"    my_tuple={my_tuple_str},\n"
+                        f"    my_frozenset={my_frozenset_str},\n"
+                        f"    my_empty_tuple={repr(())},\n"
+                        f"    my_empty_frozenset={repr(frozenset())},\n"
+                        f"    my_none=None,\n"
+                        f")"
+                    ),
+                )
