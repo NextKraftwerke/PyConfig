534fe46ba79ca8f2b75c2292daa7bf0aad654b9e
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-04-13 21:15:54 +0200

    Suppressed a few warnings.

--- nx_config/__init__.py
+++ nx_config/__init__.py
@@ -1,5 +1,10 @@
+# noinspection PyUnresolvedReferences
 from .config import Config
+# noinspection PyUnresolvedReferences
 from .secret_string import SecretString
+# noinspection PyUnresolvedReferences
 from .section import ConfigSection
+# noinspection PyUnresolvedReferences
 from .url import URL
+# noinspection PyUnresolvedReferences
 from .validation import validate
