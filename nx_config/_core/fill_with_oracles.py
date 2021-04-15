82299e9ea3c4d094406953653ef0fbcccaa1a425
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-04-15 12:38:57 +0200

    Implemented very simple parsing of env vars for base types.

--- nx_config/_core/fill_with_oracles.py
+++ nx_config/_core/fill_with_oracles.py
@@ -1,12 +1,29 @@
+from datetime import datetime
+from pathlib import Path
 from typing import Mapping
+from uuid import UUID
+
+from dateutil.parser import parse as dateutil_parse
 
 from nx_config._core.iteration_utils import get_annotations
 from nx_config._core.naming_utils import internal_name
 from nx_config._core.section_meta import run_validators
+from nx_config._core.type_checks import ConfigTypeInfo
 from nx_config._core.unset import Unset
 from nx_config.config import Config
 from nx_config.exceptions import ValidationError, IncompleteSectionError
 from nx_config.section import ConfigSection
+
+
+def _convert_string(value_str: str, type_info: ConfigTypeInfo):
+    if type_info.base in (int, float, UUID, Path):
+        return type_info.base(value_str)
+    elif type_info.base is bool:
+        return value_str == "True"
+    elif type_info.base is datetime:
+        return dateutil_parse(value_str)
+    else:
+        return value_str
 
 
 def _check_all_entries_were_set(section: ConfigSection):
@@ -20,12 +37,13 @@ def fill_config_w_oracles(cfg: Config, env_map: Mapping[str, str]):
         section = getattr(cfg, section_name)
 
         for entry_name in get_annotations(section):
-            env_key = "__".join((section_name.upper(), entry_name.upper()))
+            env_key = f"{section_name.upper()}__{entry_name.upper()}"
             new_value = env_map.get(env_key)
 
             if new_value is not None:
-                # entry = getattr(type(section), entry_name)
-                setattr(section, internal_name(entry_name), new_value)
+                type_info = getattr(type(section), entry_name).type_info
+                converted_new_value = _convert_string(new_value, type_info)
+                setattr(section, internal_name(entry_name), converted_new_value)
 
         try:
             _check_all_entries_were_set(section)
--- tests/test_fill_config_env_only.py
+++ tests/test_fill_config_env_only.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timezone, timedelta
+from pathlib import Path
 from typing import Optional
 from unittest import TestCase
+from uuid import UUID
 
-from nx_config import Config, ConfigSection, IncompleteSectionError
+from nx_config import Config, ConfigSection, IncompleteSectionError, SecretString, URL
 # noinspection PyProtectedMember
 from nx_config._core.fill_with_oracles import fill_config_w_oracles
 
@@ -76,15 +79,107 @@ class FillConfigEnvOnlyTestCase(TestCase):
         self.assertIn("'my_sec1'", msg)
         self.assertIn("'my_opt_str'", msg)
 
-    def test_fill_str(self):
+    def test_overwrite_str(self):
         class MySection(ConfigSection):
-            my_str: str = "nothing"
+            my_entry: str = "nothing"
 
         class MyConfig(Config):
             my_section: MySection
 
-        something = "something"
+        new_value = "something"
         cfg = MyConfig()
-        fill_config_w_oracles(cfg, env_map={"MY_SECTION__MY_STR": something})
+        fill_config_w_oracles(cfg, env_map={"MY_SECTION__MY_ENTRY": new_value})
 
-        self.assertEqual(something, cfg.my_section.my_str)
+        self.assertEqual(new_value, cfg.my_section.my_entry)
+
+    def test_overwrite_int(self):
+        class MySection(ConfigSection):
+            my_entry: int = 42
+
+        class MyConfig(Config):
+            my_section: MySection
+
+        new_value = "101"
+        cfg = MyConfig()
+        fill_config_w_oracles(cfg, env_map={"MY_SECTION__MY_ENTRY": new_value})
+
+        self.assertEqual(int(new_value), cfg.my_section.my_entry)
+
+    def test_fill_int(self):
+        class MySection(ConfigSection):
+            my_entry: int
+
+        class MyConfig(Config):
+            my_section: MySection
+
+        new_value = "101"
+        cfg = MyConfig()
+        fill_config_w_oracles(cfg, env_map={"MY_SECTION__MY_ENTRY": new_value})
+
+        self.assertEqual(int(new_value), cfg.my_section.my_entry)
+
+    def test_fill_all_base_types(self):
+        class MySection(ConfigSection):
+            my_int: int
+            my_float: float
+            my_bool: bool
+            my_str: str
+            my_datetime: datetime
+            my_uuid: UUID
+            my_path: Path
+            my_secret: SecretString
+            my_url: URL
+
+        class MyConfig(Config):
+            my_section: MySection
+
+        new_int_as_str = "101"
+        new_float_as_str = "3.14"
+        new_bool = False
+        new_str = "Hello there!"
+        new_datetime = datetime(2021, 5, 4, 1, 2, 3, tzinfo=timezone(offset=timedelta(hours=4)))
+        new_uuid_as_str = "5f8ae2f4-906c-4d8d-9d61-69671117027e"
+        new_path_as_str = "../some_dir/some_file.some_ext"
+        new_secret = "abcd"
+        new_url = "https://youtu.be/xvFZjo5PgG0"
+
+        cfg = MyConfig()
+        fill_config_w_oracles(
+            cfg,
+            env_map={
+                "MY_SECTION__MY_INT": new_int_as_str,
+                "MY_SECTION__MY_FLOAT": new_float_as_str,
+                "MY_SECTION__MY_BOOL": str(new_bool),
+                "MY_SECTION__MY_STR": new_str,
+                "MY_SECTION__MY_DATETIME": new_datetime.isoformat(sep="T"),
+                "MY_SECTION__MY_UUID": new_uuid_as_str,
+                "MY_SECTION__MY_PATH": new_path_as_str,
+                "MY_SECTION__MY_SECRET": new_secret,
+                "MY_SECTION__MY_URL": new_url,
+            },
+        )
+
+        self.assertEqual(int(new_int_as_str), cfg.my_section.my_int)
+        self.assertEqual(float(new_float_as_str), cfg.my_section.my_float)
+        self.assertEqual(new_bool, cfg.my_section.my_bool)
+        self.assertEqual(new_str, cfg.my_section.my_str)
+        self.assertEqual(new_datetime, cfg.my_section.my_datetime)
+        self.assertEqual(UUID(new_uuid_as_str), cfg.my_section.my_uuid)
+        self.assertEqual(Path(new_path_as_str), cfg.my_section.my_path)
+        self.assertEqual(new_secret, cfg.my_section.my_secret)
+        self.assertEqual(new_url, cfg.my_section.my_url)
+
+    # TODO:
+    #   - run tox for all python versions
+    #   - all possibilities for bool
+    #   - spaces in strings
+    #   - spaces in datetimes
+    #   - spaces in paths
+    #   - Windows-style paths
+    #   - UUIDs without hyphens
+    #   - invalid strings for each base type
+    #   - tuples
+    #   - frozensets
+    #   - optionals
+    #   - one really complex case
+    #   - document restrictions
