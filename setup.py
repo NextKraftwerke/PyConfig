78650e28e8f85a2cd2f6fda2db56721c4b7e697e
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-04-15 12:29:37 +0200

    Added python-dateutil as a dependency.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="nx_config",
     version="0.2.0",
     packages=["nx_config", "nx_config._core"],
-    install_requires=[],
+    install_requires=["python-dateutil >= 2.8.1, < 3"],
     extras_require={
         "tests": [],
         "tox": ["tox >=3.21.4, <4"],
