8d4578a621c193b61b6cb5774c3799dbb2f38df4
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-10-29 12:39:42 +0200

    Use metadata from setup.cfg in the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -12,32 +12,38 @@
 #
 # import os
 # import sys
-# sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
+from datetime import date
+from pathlib import Path
 
-project = 'PyConfig'
+from setuptools.config import read_configuration
+
+metadata = read_configuration(str(Path(__file__).parent.parent / "setup.cfg"))["metadata"]
+
+project = "PyConfig"
+author = metadata["author"]
 # noinspection PyShadowingBuiltins
-copyright = '2021, Tomás Silveira Salles'  # TODO: Remove? License instead?
-author = 'Tomás Silveira Salles'  # TODO: Get from setup.cfg
-release = '0.2.0-beta.1'  # TODO: Get from setup.cfg
+copyright = f"{date.today().year} by {author}"
+release = metadata["version"]
 
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -45,9 +51,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'python_docs_theme'
+html_theme = "python_docs_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
