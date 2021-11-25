bb15af4eca241163b1c055a7895e9e60f12e0370
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-11-25 12:51:17 +0100

    Use sphinx extension githubpages.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ release = metadata["version"]
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.githubpages"]
 
 autodoc_default_options = {
     "imported-members": False,
