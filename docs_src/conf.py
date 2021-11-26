c1154be07f0971bf921c7d7b2467087ef27b048f
Tomás Silveira Salles <30598365+tomasssalles@users.noreply.github.com>
2021-11-26 10:40:23 +0100

    Fixed "last updated" stamp at bottom of docs.

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -52,5 +52,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = "python_docs_theme"
+
+# If not None, a 'Last updated on:' timestamp is inserted at every page
+# bottom, using the given strftime format.
+html_last_updated_fmt = "%b %d, %Y"
